5e574ebcead6d56c2fa15b60aacc441a6e13a616
Aaron <aarlin5409@hessleacademy.com>
2020-11-24 12:38:11 +0000

    Add project files.

---
+++ SearchSortAlgorithms/Searching/LinearSearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SearchSortAlgorithms.Searching
+{
+    public static partial class Algorithm
+    {
+        /// <summary>
+        /// Finds an element in an array by searching sequentially
+        /// </summary>
+        /// <typeparam name="T">Type of data</typeparam>
+        /// <param name="arr">Array to search</param>
+        /// <param name="elem">Elem to search for</param>
+        /// <returns>Index of <paramref name="elem"/> (-1 if not in array)</returns>
+        public static int LinearSearch<T>(this T[] arr, T elem)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i].Equals(elem))
+                    return i;
+
+            return -1;
+        }
+    }
+}
---
+++ SearchSortAlgorithmsTests/Searching/LinearSearchTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SearchSortAlgorithms.Searching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchSortAlgorithms.Searching.Tests
+{
+    [TestClass()]
+    public class LinearSearchTests
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 3, 2, 5, 8 }, 5, 2, DisplayName = "Simple search")]
+        [DataRow(new int[] { 6, 2, 753, 51, 653, 86545 }, 653, 4, DisplayName = "Longer search")]
+        [DataRow(new int[] { 3, 2, 5, 8 }, 55, -1, DisplayName = "Fail search")]
+        public void LinearSearchIntTest(int[] arr, int elem, int pos)
+        {
+            var result = arr.LinearSearch(elem);
+
+            Assert.AreEqual(pos, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(new string[] { "a", "b", "ab", "ba" }, "ab", 2, DisplayName = "Simple search")]
+        [DataRow(new string[] { "a", "b", "ab", "ba" }, "abc", -1, DisplayName = "Fail search")]
+        public void LinearSearchStringTest(string[] arr, string elem, int pos)
+        {
+            var result = arr.LinearSearch(elem);
+
+            Assert.AreEqual(pos, result);
+        }
+    }
+}
